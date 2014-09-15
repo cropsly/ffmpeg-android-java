566c46a4ebd6db57ebe834d7810d529da7f648ba
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-15 17:03:00 +0530

    Added test class for ShellCommand.java

---
+++ FFmpegAndroid/src/androidTest/java/com/github/hiteshsondhi88/libffmpeg/ShellCommandTest.java
@@ -0,0 +1,12 @@
+package com.github.hiteshsondhi88.libffmpeg;
+
+public class ShellCommandTest extends CommonTestCase {
+
+    public void testRun() throws Exception {
+        // TODO
+    }
+
+    public void testRunWaitFor() throws Exception {
+        // TODO
+    }
+}
