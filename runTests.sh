d0b76a4c9fa1f3a39019dced399b891359219537
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-18 18:03:07 +0530

    updating sdk before running tests #travis

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# update android sdk
+android update sdk --no-ui --force
+
 # x86 emulator
 echo no | android create avd -c 50M --force -n testx86 -t android-16 --abi x86
 emulator -ports 5554,5555 -partition-size 256 -avd testx86 -no-skin -no-boot-anim -no-audio -no-window &
@@ -13,6 +16,5 @@ PID_EMU2=$!
 ./wait_for_emulator emulator-5556
 
 # Running Tests
-adb -s emulator-5554 logcat &
 ./gradlew --info build connectedCheck || exit 1
 kill -9 $PID_EMU1 $PID_EMU2
