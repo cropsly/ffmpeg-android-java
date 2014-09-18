5ae7b143ec8903769cbb35dabb10a17dbf325166
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-18 20:24:33 +0530

    Updated x86 asset

--- FFmpegAndroid/src/androidTest/java/com/github/hiteshsondhi88/libffmpeg/CpuArchHelperTest.java
+++ FFmpegAndroid/src/androidTest/java/com/github/hiteshsondhi88/libffmpeg/CpuArchHelperTest.java
@@ -20,5 +20,4 @@ public class CpuArchHelperTest extends TestCase {
         }
     }
 
-
 }
--- FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/CpuArch.java
+++ FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/CpuArch.java
@@ -3,7 +3,7 @@ package com.github.hiteshsondhi88.libffmpeg;
 import android.text.TextUtils;
 
 enum CpuArch {
-    x86("f04974831890b3750761e487f3e4b0d9bdb688a1"),
+    x86("a93cdd533ec9b8865aed20e625ba01515c86c5c7"),
     ARMv7("3f4580bce9187f94b6cf8ef530074ef0975cb476"),
     ARMv7_NEON("fc544c42cf0ef993c0b55463807e5c68cd323c93"),
     NONE(null);
