2e7c1bbf6ae3fde9d31ad37448681b91f70d6366
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-18 19:01:15 +0530

    Updated assets

--- FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/CpuArch.java
+++ FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/CpuArch.java
@@ -3,15 +3,19 @@ package com.github.hiteshsondhi88.libffmpeg;
 import android.text.TextUtils;
 
 enum CpuArch {
-    x86("704756b89ba6c7ed5b95758c7064fee2f644be17"),
-    ARMv7("a25f433a088e197aae10cadd34bcbf2660a45d07"),
-    ARMv7_NEON("edb801d23dcf6993b3f498a878520c539ea40f30"),
+    x86("8eb744a10148314e5be8f2b538782a0ce3363fc1"),
+    ARMv7("1224f5a941482b56c16f89a97b6b79ba0ff03804"),
+    ARMv7_NEON("b46a0e1145ba7f59d69bb93c0b3348c58c735870"),
     NONE(null);
 
     private String sha1;
 
     CpuArch(String sha1) {
         this.sha1 = sha1;
+    }
+
+    String getSha1(){
+        return sha1;
     }
 
     static CpuArch fromString(String sha1) {
