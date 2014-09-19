341e7e1a007238e3e91a101183bf0a6bdfc33eb5
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-19 21:50:22 +0530

    Updated Tests script

--- runTests.sh
+++ runTests.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-# building project for the first time
-./gradlew --info clean build || exit 1
-
 function run_test() {
   abi=$1
   api_level=$2
   emulator_name="test_${abi}_${api_level}"
+  echo "==============================================="
+  echo "      Starting Tests for ${emulator_name}      "
+  echo "==============================================="
   echo no | android create avd -c 50M --force -n ${emulator_name} -t ${api_level} --abi ${abi}
   emulator -partition-size 256 -avd ${emulator_name} -no-skin -no-boot-anim -no-audio -no-window -gpu on &
   EMU_PID=$!
   ./wait_for_emulator || exit 1
-  adb shell input keyevent 82 &
-  
+  adb -v time logcat -s FFmpegInstrumentationTest &
+  LOGCAT_PID=$!
   # Running Tests
-  ./gradlew --info connectedCheck || exit 1
+  ./gradlew --info build connectedCheck || exit 1
   
   # Killing emulator
-  kill -9 $EMU_PID
+  kill -9 $EMU_PID $LOGCAT_PID
   
   # delete emulator
   sleep 5 # wait for emulator to get killed
   android delete avd -n ${emulator_name} # delete emulator 
+  echo "==============================================="
+  echo "      Finished Tests for ${emulator_name}      "
+  echo "==============================================="
 }
 
 # x86 android-16
