f76e85a3e5263e1042c7ac52fb67857e7dff1331
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-19 19:22:23 +0530

    Covering Android L x86 and arm emulators in test cases

--- runTests.sh
+++ runTests.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+# building project for the first time
+./gradlew --info clean build || exit 1
+
 function run_test() {
   abi=$1
   api_level=$2
-  port1=$3
-  port2=$4
   emulator_name="test_${abi}_${api_level}"
-  emulator_serial="emulator-${port1}"
-  echo no | android create avd -c 50M --force -n $emulator_name -t ${api_level} --abi ${abi}
-  emulator -ports ${port1},${port2} -partition-size 256 -avd $emulator_name -no-skin -no-boot-anim -no-audio -no-window -gpu on &
+  echo no | android create avd -c 50M --force -n ${emulator_name} -t ${api_level} --abi ${abi}
+  emulator -partition-size 256 -avd ${emulator_name} -no-skin -no-boot-anim -no-audio -no-window -gpu on &
   EMU_PID=$!
-  ./wait_for_emulator ${emulator_serial} || exit 1
-  adb -s ${emulator_serial} shell input keyevent 82 &
-  kill -9 $EMU_PID
+  ./wait_for_emulator || exit 1
+  adb shell input keyevent 82 &
   
   # Running Tests
-  ./gradlew --info clean build connectedCheck || exit 1
+  ./gradlew --info connectedCheck || exit 1
+  
+  # Killing emulator
+  kill -9 $EMU_PID
+  
+  # delete emulator
+  sleep 5 # wait for emulator to get killed
+  android delete avd -n ${emulator_name} # delete emulator 
 }
 
 # x86 android-16
-run_test x86 android-16 5554 5555
+run_test x86 android-16
 
 # armeabi-v7a android-16
-run_test armeabi-v7a android-16 5556 5557 
+run_test armeabi-v7a android-16
 
+# x86 android-L
+run_test x86 android-L
+
+# armeabi-v7a android-L 
+run_test armeabi-v7a android-L
