834a64f97de676088124c3df603328ffab280484
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-19 13:34:30 +0530

    Updated test script

--- runTests.sh
+++ runTests.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# x86 emulator
-echo no | android create avd -c 50M --force -n testx86 -t android-16 --abi x86
-emulator -ports 5554,5555 -partition-size 256 -avd testx86 -no-skin -no-boot-anim -no-audio -no-window -gpu on &
-PID_EMU1=$!
-./wait_for_emulator emulator-5554 || exit 1
-adb -s emulator-5554 shell input keyevent 82 &
+function create_emulator() {
+  abi=$1
+  api_level=$2
+  port1=$3
+  port2=$4
+  emulator_name="test_${abi}_${api_level}"
+  emulator_serial="emulator-${port1}"
+  echo no | android create avd -c 50M --force -n $emulator_name -t ${api_level} --abi ${abi}
+  emulator -ports ${port1},${port2} -partition-size 256 -avd $emulator_name -no-skin -no-boot-anim -no-audio -no-window -gpu on &
+  ./wait_for_emulator ${emulator_serial} || exit 1
+  adb -s ${emulator_serial} shell input keyevent 82 &
+}
 
-# armeabi-v7a emulator
-echo no | android create avd -c 50M --force -n testarm -t android-16 --abi armeabi-v7a
-emulator -ports 5556,5557 -partition-size 256 -avd testarm -no-skin -no-boot-anim -no-audio -no-window -gpu on &
-PID_EMU2=$!
-./wait_for_emulator emulator-5556 || exit 1
-adb -s emulator-5556 shell input keyevent 82 &
+# x86 emulator android-16
+create_emulator x86 android-16 5554 5555
+
+# armeabi-v7a emulator android-16
+create_emulator armeabi-v7a android-16 5556 5557 
 
 # Running Tests
 ./gradlew --info build connectedCheck || exit 1
-kill -9 $PID_EMU1 $PID_EMU2
